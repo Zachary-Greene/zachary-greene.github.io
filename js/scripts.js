02fdf474e92e5ab58d3b5b9d402182174270cf77
Zachary Greene <zacharygreene01@gmail.com>
2020-08-25 19:45:17 +1000

    First commit of files

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20)
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+  });
+
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+
+  $(".works").magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{enabled:true}
+  });
+
+  new Glide(".images", {
+      type: "carousel",
+      perView: 3,
+      gap: 10,
+      autoplay: 1,
+      animationDuration: 15000,
+      animationTimingFunc: 'linear',
+      breakpoints: {
+          1200:{
+              perView: 3
+          },
+          800:{
+              perview: 1
+          }
+      }
+  }).mount();
+});
+
