eb106df09a6b48dae08731011cd665bb1badb89d
Zachary Greene <zacharygreene01@gmail.com>
2021-07-03 20:34:40 +1000

    Changed Files - Quality Of Life
    Removed Project files, updated Index.html & Portfolio.html, made more mobile friendly, updated corresponding CSS files and JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,77 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20)
-      $(".navbar").addClass("sticky");
-    else
-      $(".navbar").removeClass("sticky");
-  });
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20)
+            $(".navbar").addClass("sticky");
+        else
+            $(".navbar").removeClass("sticky");
+    });
 
-  $('.menu-toggler').click(function(){
-    $(this).toggleClass("active");
-    $(".navbar-menu").toggleClass("active");
-  });
+    $('.menu-toggler').click(function() {
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
 
-  $(".works").magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery:{enabled:true}
-  });
+    $(".works").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: { enabled: true }
+    });
 
-//   new Glide(".images", {
-//       type: "carousel",
-//       perView: 3,
-//       gap: 10,
-//       autoplay: 1,
-//       variableWidth: true,
-//       animationDuration: 15000,
-//       animationTimingFunc: 'linear',
-//       breakpoints: {
-//           1200:{
-//               perView: 3
-//           },
-//           800:{
-//               perview: 1
-//           }
-//       }
-//   }).mount();
+    // Heading Animation
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function() {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; color: #fff}"
+        document.body.appendChild(css);
+    };
 });
-
